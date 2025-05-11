d70808c8430e297e28d072263a0f537cab8eb648
BoomTrading <orytami@gmail.com>
2025-05-12 01:02:18 +0200

    first commit

---
+++ src/main/java/com/example/LogicBro/LogicBroApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogicBro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicBroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicBroApplication.class, args);
+	}
+
+}
